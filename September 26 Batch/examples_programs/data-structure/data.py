116868af5f91feb9fd0df07b627a1e5bf1e43449
Abhinav Awasthi <awasthiabhinav744@gmail.com>
2026-09-12 12:36:12 +0530

    feat(data-structure): add examples for lists, dictionaries, and zipping data feat(f-strings): implement function to generate prompts using f-strings fix(requirements): correct http package name to httpx

---
+++ September 26 Batch/examples_programs/data-structure/data.py
@@ -0,0 +1,16 @@
+# list
+students = ["Abhinav", "Aman", "Rahul", "Yash"]
+print(students)
+print(students[2])
+print(students[-1])
+
+for i in students:
+    print(i)
+
+# dictionary
+details = {
+    "Batch": "AI September Batch",
+    "Topic": "Python",
+    "Date": "12th Sep"
+}
+print(details)
